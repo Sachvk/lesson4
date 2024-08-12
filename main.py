38ecc3c7e86d873515b16fe0c38ade8a2378fe99
Sachvk <serg22011985@gmail.com>
2024-08-12 15:48:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
 homework = 12
 hour = 1.5
 name_lesson = "Pyhton"
-result=(1.5/12)
+result=(homework/hour)
 print(name_lesson,', всего задач:', homework ,', затрачено часов:', hour, ', среднее время выполнения ', result, 'часа' )
